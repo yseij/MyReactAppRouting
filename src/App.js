97aadfde4f5771b0924f007fb514f2af25e6edb7
yseij <r0790156@student.thomasmore.be>
2022-01-26 18:56:37 +0100

    url routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorite";
+
 function App() {
   return (
-    <div></div>);
+    <div>
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage/>
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+    );
 }
 
 export default App;
