c12080bb4620e9bfbcff10361d11f82991b235f7
yseij <r0790156@student.thomasmore.be>
2022-01-29 21:42:14 +0100

    Post request to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupsPage(){
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm/>
+function NewMeetupsPage() {
+  function onAddMeetup(meetupData) {
+    fetch(
+      "https://react-beef5-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+          method: 'POST',
+          body: JSON.stringify(meetupData),
+          headers: {
+              'Content-Type' : 'application/json'
+          }
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={onAddMeetup} />
     </section>
+  );
 }
 
 export default NewMeetupsPage;
