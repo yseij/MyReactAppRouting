26a1cd57b57235438ac26f827c7f532c46d45ba4
yseij <r0790156@student.thomasmore.be>
2022-01-29 20:21:35 +0100

    meer componenten voor styling

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,21 @@
+import classes from './MeetupItem.module.css'
+
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div>
+        <button>To Favorites</button>
+      </div>
+    </li>
+  );
+}
+
+export default MeetupItem;
