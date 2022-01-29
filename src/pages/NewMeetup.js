a88b8dc1c37f3a1ae6520630e320f5b1c9217f2a
yseij <r0790156@student.thomasmore.be>
2022-01-29 21:04:05 +0100

    forms

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage(){
-    return <div>New Meetups Page</div>
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm/>
+    </section>
 }
 
 export default NewMeetupsPage;
