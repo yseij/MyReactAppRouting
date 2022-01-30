7c4229ae3ad062635d07915a99bdfc6e159800ed
yseij <r0790156@student.thomasmore.be>
2022-01-30 14:43:35 +0100

    context nog meer gebruiken

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,5 +1,23 @@
+import {useContext} from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+import FavoritesContext from "../store/favorites-context";
+
 function FavoritesPage(){
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext)
+
+    let content
+
+    if(favoritesCtx.totalFavorites === 0){
+        content = <p>you got no favorites, Add some</p>
+    }else {
+        content = <MeetupList meetups={favoritesCtx.favorites}/>
+    }
+
+    return <section>
+        <h1>My favorites</h1>
+        {content}
+    </section>
 }
 
 export default FavoritesPage;
